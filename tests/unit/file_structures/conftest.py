afc87ca9af6f8c0b9e9d9adcdebc0b16c12d5a21
Christoph Becker <christoph.becker@durham.ac.uk>
2020-12-22 15:19:14 +0000

    update gitignore, conftest

--- tests/unit/file_structures/conftest.py
+++ tests/unit/file_structures/conftest.py
@@ -8,19 +8,25 @@ dir_sim = "/tmp/"
 
 #@pytest.fixture(scope="session", autouse=True)
 def pytest_sessionstart(session):
-    for snap_nr in range(1, 12):
-        dir_name = dir_sim + "rockstar_%03d/" % snap_nr
-        os.mkdir(dir_name)
-        for file_nr in range(1, 8):
-            filename = dir_name + "halos_0.%d.ascii" % file_nr
-            open(filename, 'w+b')
+    for box_nr in range(1, 5):
+        dir_box = dir_sim + "box%d/" % box_nr
+        os.mkdir(dir_box)
+        for snap_nr in range(1, 12):
+            dir_rock = dir_box + "rockstar_%03d/" % snap_nr
+            os.mkdir(dir_rock)
+            for file_nr in range(1, 8):
+                filename = dir_rock + "halos_0.%d.ascii" % file_nr
+                open(filename, 'w+b')
 
 
 #@pytest.fixture(scope="session")
 def pytest_sessionfinish(session, exitstatus):
-    for snap_nr in range(1, 12):
-        dir_name = "/tmp/rockstar_%03d/" % snap_nr
-        for file_nr in range(1, 8):
-            filename = dir_name + "halos_0.%d.ascii" % file_nr
-            os.remove(filename)
-        os.rmdir(dir_name)
+    for box_nr in range(1, 5):
+        dir_box = dir_sim + "box%d/" % box_nr
+        for snap_nr in range(1, 12):
+            dir_rock = dir_box + "/rockstar_%03d/" % snap_nr
+            for file_nr in range(1, 8):
+                filename = dir_rock + "halos_0.%d.ascii" % file_nr
+                os.remove(filename)
+            os.rmdir(dir_rock)
+        os.rmdir(dir_box)
