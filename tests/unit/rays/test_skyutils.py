9922fbc0a459a9a70065cc73a3f5f4058fda9225
Christoph Becker <christoph.becker@durham.ac.uk>
2021-01-06 15:03:54 +0000

    sky_utils tests for NFW analytic results

--- tests/unit/rays/test_skyutils.py
+++ tests/unit/rays/test_skyutils.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import pandas as pd
 import numpy as np
 import numpy.testing as npt
+from scipy import signal
 
 from astropy import units as un
 from astropy.constants import sigma_T, m_p, c
@@ -33,10 +34,65 @@ def create_mock_skymap_dataframe():
 
 @pytest.fixture(name="map_np", scope="module")
 def create_mock_skymap_ndarray():
-    map_np = np.ones((100, 100))
-    map_np[40:60, 40:60] = 10
-    return map_np
+    return np.outer(
+        signal.general_gaussian(100, p=1, sig=10),
+        signal.general_gaussian(100, p=1, sig=10),
+    )
 
+
+def test__analytic_Halo_dT_signal_to_SkyArray():
+    halo_dic = {
+        "r200_deg": np.array([0.05]),
+        "r200_pix": np.array([50]),
+        "m200": np.array([7e13]),
+        "c_NFW": np.array([2.]),
+        "Dc": np.array([1050]),
+        "theta1_pix": np.array([200]),
+        "theta2_pix": np.array([200]),
+        "theta1_vel": np.array([200]),
+        "theta2_vel": np.array([200]),
+    }
+    map_np = SkyUtils.analytic_Halo_signal_to_SkyArray(
+        halo_idx=np.array([0]),
+        halo_cat=halo_dic,
+        extent=20,
+        direction=[0],
+        suppress=True,
+        suppression_R=10,
+        npix=400,
+        signal="dT",
+    )
+    assert np.unravel_index(map_np.argmax(), map_np.shape) == (200, 167)
+    npt.assert_almost_equal(map_np.min(), -2.0699e-08, decimal=10)
+    npt.assert_almost_equal(map_np.mean(), 2.4732e-11, decimal=12)
+    npt.assert_almost_equal(map_np.max(), 2.0699e-08, decimal=10)
+
+def test__analytic_Halo_alpha_signal_to_SkyArray():
+    halo_dic = {
+        "r200_deg": np.array([0.05]),
+        "r200_pix": np.array([50]),
+        "m200": np.array([7e13]),
+        "c_NFW": np.array([2.]),
+        "Dc": np.array([1050]),
+        "theta1_pix": np.array([200]),
+        "theta2_pix": np.array([200]),
+        "theta1_vel": np.array([200]),
+        "theta2_vel": np.array([200]),
+    }
+    map_np = SkyUtils.analytic_Halo_signal_to_SkyArray(
+        halo_idx=np.array([0]),
+        halo_cat=halo_dic,
+        extent=20,
+        direction=[0],
+        suppress=True,
+        suppression_R=10,
+        npix=400,
+        signal="alpha",
+    )
+    assert np.unravel_index(map_np.argmax(), map_np.shape) == (200, 233)
+    npt.assert_almost_equal(map_np.min(), -3.1027e-05, decimal=8)
+    npt.assert_almost_equal(map_np.mean(), -3.7073e-08, decimal=11)
+    npt.assert_almost_equal(map_np.max(), 3.1027e-05, decimal=8)
 
 def test__convert_code_to_phy_units_of_shear(map_df):
     map_df = SkyUtils.convert_code_to_phy_units("shear_x", map_df)
@@ -58,7 +114,12 @@ def test__convert_convergence_to_deflection_ctypes(map_np):
     alpha_1, alpha_2 = SkyUtils.convert_convergence_to_deflection_ctypes(
         map_np, 100, 10 * un.deg,
     )
-    print(np.argmax(alpha_1, axis=1), alpha_1.shape)
-    assert np.argmax(alpha_1, axis=1) == 50
-    assert np.argmin(alpha_1) == (50, 50)
-
+    npt.assert_almost_equal(
+        alpha_1.min(), -0.0146571, decimal=6,
+    )
+    npt.assert_almost_equal(
+        alpha_1.mean(), 6.49e-05, decimal=6,
+    )
+    npt.assert_almost_equal(
+        alpha_1.max(), 0.0151880, decimal=6,
+    )
