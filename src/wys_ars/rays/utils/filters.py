a8b62bcd139c14a34fefde6b96793316a16c0181
Christoph Becker <christoph.becker@durham.ac.uk>
2021-01-08 13:58:36 +0000

    added aperture photometry, fwhm to sigma conversion

--- src/wys_ars/rays/utils/filters.py
+++ src/wys_ars/rays/utils/filters.py
@@ -23,16 +23,53 @@ class Filters:
     fields.
 
     Methods:
+        aperture_photometry:
         dictionary_learning:
         pca:
         gaussian:
-        gaussian_low_pass:
         gaussian_high_pass:
+        gaussian_field:
         gaussian_compensated:
         apodization:
         gaussian_third_derivative:
         tophat_compensated:
+        fwhm_to_sigma:
+        sigma_to_fwhm:
     """
+
+    def aperture_photometry(
+        img: np.ndarray,
+        theta: un.quantity.Quantity,
+        alpha: un.quantity.Quantity,
+    ) -> np.ndarray:
+        """
+        Take the average value of the pixels belonging to the disk and
+        substract from this average the average value of pixels in a
+        ring with bounds [alpha, alpha*sqrt(2)]. This is useful for
+        ISW and RS analyzes when long wave-modes are involved, e.g. due to CMB.
+        arxiv: 1607.02139, Sec. III.B
+
+        Args:
+            img: flat 2D SkyArray-image
+            theta: opening angle of image (fov), [some angular distance]
+            alpha: aperture, for kSz it can be 1.8 arcmin
+        Returns:
+        """
+        # get distances of pixels in units of pixels
+        _npix = len(img)
+        x1edge = np.linspace(1, _npix, _npix) - _npix / 2 - 0.5
+        x, y = np.meshgrid(x1edge, x1edge)
+        pixel_dist = np.sqrt(x ** 2 + y ** 2)
+        # convert aperture unit to pixels
+        pix_per_deg = _npix / theta.to(un.deg).value
+        alpha_pix = np.ceil(alpha.to(un.deg).value * pix_per_deg).astype("int")
+        # get pixels in ring
+        indx_ring = np.logical_and(
+            alpha_pix < pixel_dist, pixel_dist < alpha_pix*np.sqrt(2),
+        )
+        img -= np.mean(img[indx_ring])
+        return img
+
 
     def dictionary_learning(
         clean_data: np.ndarray,
@@ -82,6 +119,7 @@ class Filters:
         dt = time() - t0
         return cleaned_img
 
+
     def pca(tiles: np.ndarray, n_components: int = 5) -> np.ndarray:
         """
         CMB fore- and background seperation. Important for detection
@@ -105,6 +143,7 @@ class Filters:
         components = pca.transform(tiles)
         cleaned_tiles = pca.inverse_transform(components)
         return cleaned_tiles.reshape(ntiles, npix, npix)
+
 
     def apodization(
         img: np.ndarray, theta: float, theta_i: Optional[float] = None
@@ -130,10 +169,12 @@ class Filters:
         )
         return img * window
 
+
     def gaussian(
         img: np.ndarray,
         theta: un.quantity.Quantity,
-        theta_i: un.quantity.Quantity,
+        theta_i: Optional[un.quantity.Quantity] = None,
+        fwhm_i: Optional[un.quantity.Quantity] = None,
         **kwargs,
     ) -> np.ndarray:
         """
@@ -146,25 +187,39 @@ class Filters:
         Args:
             img: partial sky-map
             theta: edge-lenght of field-of-view [deg]
-            theta_i: smoothing kernel width [arcmin]
+            theta_i: sigma of gaussian used to indicated
+                smoothing kernel width, [arcmin]
+            fwhm_i: Full-Width-Half-Maximum (fwhm) of gaussian used to indicated
+                smoothing kernel width, [arcmin]
+
+        Returns:
         """
         img = ConvergenceMap(data=img, angle=theta.to(un.deg))
+        if theta_i is None and fwhm_i is None:
+            raise ValueError(f"Either theta_i or fwhm_i must be set for smoothing scale.")
+        elif theta_i is None:
+            sigma_i = Filters.fwhm_to_sigma(fwhm_i)
+        else:
+            sigma_i = theta_i
+
         if len(img.data) < 500:
             # for image with less than 500^2 images real-space is faster
             img = img.smooth(
-                scale_angle=theta_i.to(un.arcmin), kind="gaussian", **kwargs
+                scale_angle=sigma_i.to(un.arcmin), kind="gaussian", **kwargs
             )
         else:
             # for larger images FFT is optimal
             img = img.smooth(
-                scale_angle=theta_i.to(un.arcmin), kind="gaussianFFT", **kwargs
+                scale_angle=sigma_i.to(un.arcmin), kind="gaussianFFT", **kwargs
             )
         return img.data
+
 
     def gaussian_high_pass(
         img: np.ndarray,
         theta: un.quantity.Quantity,
-        theta_i: un.quantity.Quantity,
+        theta_i: Optional[un.quantity.Quantity] = None,
+        fwhm_i: Optional[un.quantity.Quantity] = None,
     ) -> np.ndarray:
         """
         Gaussian high-pass filter
@@ -172,13 +227,27 @@ class Filters:
         Args:
             img: partial sky-map
             theta: edge-lenght of field-of-view [deg]
-            theta_i: smoothing kernel width [arcmin]
+            theta_i: sigma of gaussian used to indicated
+                smoothing kernel width, [arcmin]
+            fwhm_i: Full-Width-Half-Maximum (fwhm) of gaussian used to indicated
+                smoothing kernel width, [arcmin]
+        
+        Returns:
         """
-        lowpass = Filters.gaussian(img, theta, theta_i)
+        lowpass = Filters.gaussian(img, theta, theta_i, fwhm_i)
         highpass = img - lowpass
         return highpass
 
-    def gaussian_third_derivative_1(
+
+    def sigma_to_fwhm(sigma: float) -> float:
+        return sigma * (2*np.sqrt(2*np.log(2)))
+
+
+    def fwhm_to_sigma(fwhm: float) -> float:
+        return fwhm / (2*np.sqrt(2*np.log(2)))
+
+
+    def gaussian_third_derivative_convolution(
         img: np.ndarray,
         theta: un.quantity.Quantity,
         theta_i: un.quantity.Quantity,
@@ -225,38 +294,34 @@ class Filters:
         )
         return gauss_1 - gauss_2 + gauss_3
 
-    def gaussian_third_derivative_2(
+    def gaussian_third_derivative(
         img: np.ndarray,
         theta: un.quantity.Quantity,
         theta_i: un.quantity.Quantity,
         direction: int,
     ) -> np.ndarray:
         """
-        Omni-directional third derivative gaussian kernel (also called DGD3 filter),
-        useful for extracting dipole signal, based on
+        Omni-directional 3-order derivative gaussian kernel
+        (also called DGD1 filter), useful for extracting dipole signal.
+        Based on:
         DOI: 10.3847/2041-8213/ab0bfe
         arXiv: 1812.04241
 
         Args:
             img: partial sky-map
-            theta: [some angular distance]
+            theta: opening angle of image (fov), [some angular distance]
             theta_i: [some angular distance]
             sigma: ideally it should be the width of halo, R200. units are [pix]
         """
-
-        def _gauss_dist(theta: np.ndarray, sigma: int) -> np.ndarray:
-            return np.exp(-theta ** 2 / (2 * sigma ** 2)) / (
-                2 * np.pi * sigma ** 2
-            )
 
         def _create_dgd3(
             dist: np.ndarray, theta_fov: float, theta_i: int, axis: int
         ) -> np.ndarray:
             """ Filters Function """
             gauss = (
-                _gauss_dist(dist, theta_i * 0.5)
-                - _gauss_dist(dist, theta_i) * 0.75
-                + _gauss_dist(dist, theta_i * 2.0) * 0.5
+                Filters.gaussian_field(dist, theta_i * 0.5)
+                - Filters.gaussian_field(dist, theta_i)
+                + Filters.gaussian_field(dist, theta_i * 2.0)
             )
             d1_gauss = np.gradient(
                 gauss, theta_fov / len(dist), axis=axis, edge_order=2
@@ -277,8 +342,9 @@ class Filters:
         theta_i_pix = np.ceil(
             _npix * theta_i.to(un.deg).value / theta.to(un.deg).value
         ).astype("int")
-        window = abs(_create_dgd3(dist, theta_fov_deg, theta_i_pix, direction))
+        window = _create_dgd3(dist, theta_fov_deg, theta_i_pix, direction)
         return np.multiply(window, img)
+        
 
     def gaussian_first_derivative(
         img: np.ndarray,
@@ -287,29 +353,25 @@ class Filters:
         direction: int,
     ) -> np.ndarray:
         """
-        Omni-directional third derivative gaussian kernel (also called DGD3 filter),
-        useful for extracting dipole signal, based on
+        Omni-directional 1-order derivative gaussian kernel
+        (also called DGD1 filter), useful for extracting dipole signal.
+        Based on:
         DOI: 10.3847/2041-8213/ab0bfe
         arXiv: 1812.04241
 
         Args:
             img: partial sky-map
-            theta: [some angular distance]
+            theta: opening angle of image (fov), [some angular distance]
             theta_i: [some angular distance]
             sigma: ideally it should be the width of halo, R200. units are [pix]
         """
-
-        def _gauss_dist(theta: np.ndarray, sigma: int) -> np.ndarray:
-            return np.exp(-theta ** 2 / (2 * sigma ** 2)) / (
-                2 * np.pi * sigma ** 2
-            )
 
         def _create_dgd3(
             dist: np.ndarray, theta_fov: float, theta_i: int, axis: int
         ) -> np.ndarray:
             """ Filters Function """
             d1_gauss = np.gradient(
-                _gauss_dist(dist, theta_i * 0.5),
+                Filters.gaussian_field(dist, theta_i * 0.5),
                 theta_fov / len(dist),
                 axis=axis,
                 edge_order=2,
@@ -326,6 +388,19 @@ class Filters:
         ).astype("int")
         window = _create_dgd3(dist, theta_fov_deg, theta_i_pix, direction)
         return np.multiply(window, img)
+   
+
+    def gaussian_field(theta: np.ndarray, sigma: int) -> np.ndarray:
+        """
+        Args:
+            theta: 2D array containing distances to image centre of each pixel.
+            sigma: width of Gaussian.
+        Returns:
+        """
+        return np.exp(-theta ** 2 / (2 * sigma ** 2)) / (
+            2 * np.pi * sigma ** 2
+        )
+
 
     def gaussian_compensated(
         img: np.ndarray,
