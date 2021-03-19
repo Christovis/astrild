bb15f2d392842f9b32de12b5db5c86079bc97105
Christoph Becker <christoph.becker@durham.ac.uk>
2021-03-19 12:12:52 +0000

    add filters to tests

--- tests/unit/rays/utils/test_filters.py
+++ tests/unit/rays/utils/test_filters.py
@@ -16,24 +16,26 @@ from astrild.rays.skys import SkyUtils
 dir_test_base = Path(__file__).parent.parent.parent.absolute()
 dir_test_data = dir_test_base / "test_data"
 file_test_skymap = dir_test_data / "Ray_maps_zrange_0.08_0.90.h5"
+halo_dic = {
+    "r200_deg": np.array([0.05]),
+    "r200_pix": np.array([50]),
+    "m200": np.array([7e13]),
+    "c_NFW": np.array([2.]),
+    "Dc": np.array([1050]),
+    "theta1_pix": np.array([200]),
+    "theta2_pix": np.array([200]),
+    "theta1_tv": np.array([200]),
+    "theta2_tv": np.array([200]),
+}
+extent = 20
+theta = halo_dic["r200_deg"][0]*extent
 
 @pytest.fixture(name="img", scope="module")
 def test__analytic_Halo_dT_signal_to_SkyArray():
-    halo_dic = {
-        "r200_deg": np.array([0.05]),
-        "r200_pix": np.array([50]),
-        "m200": np.array([7e13]),
-        "c_NFW": np.array([2.]),
-        "Dc": np.array([1050]),
-        "theta1_pix": np.array([200]),
-        "theta2_pix": np.array([200]),
-        "theta1_tv": np.array([200]),
-        "theta2_tv": np.array([200]),
-    }
     dT_map = SkyUtils.analytic_Halo_signal_to_SkyArray(
         halo_idx=np.array([0]),
         halo_cat=halo_dic,
-        extent=20,
+        extent=extent,
         direction=[0],
         suppress=True,
         suppression_R=10,
@@ -43,15 +45,40 @@ def test__analytic_Halo_dT_signal_to_SkyArray():
     return dT_map
 
 def test__gaussian(img):
-    theta = 10. * un.deg
     fwhm_i = 10 * un.arcmin
-    fimg = Filters.gaussian(img, theta=theta, fwhm_i=fwhm_i)
-    npt.assert_almost_equal(fimg.max(), 2.060085e-08, decimal=14)
-
+    fimg = Filters.gaussian(img, theta=theta*un.deg, fwhm_i=fwhm_i)
+    npt.assert_almost_equal(fimg.max() * 1e8, 1.665952, decimal=5)
 
 def test__gaussian_high_pass(img):
-    theta = 10. * un.deg
     fwhm_i = 5 * un.arcmin
-    fimg = Filters.gaussian(img, theta=theta, fwhm_i=fwhm_i)
-    npt.assert_almost_equal(fimg.max(), 2.0674156e-08, decimal=14)
+    fimg = Filters.gaussian(img, theta=theta*un.deg, fwhm_i=fwhm_i)
+    npt.assert_almost_equal(fimg.max() * 1e8, 1.901196, decimal=5)
+
+def test__sigma_to_fwhm():
+    sigma = 1 / (2*np.sqrt(2*np.log(2)))
+    fwhm = Filters.sigma_to_fwhm(sigma)
+    assert fwhm == 1
+
+def test__fwhm_to_sigma():
+    fwhm = (2*np.sqrt(2*np.log(2)))
+    sigma = Filters.fwhm_to_sigma(fwhm)
+    assert sigma == 1
+
+def test__gaussian_third_derivative(img):
+    # test dipole filter along 0-direction
+    fimg = Filters.gaussian_third_derivative(
+        img, theta=theta*un.deg, theta_i=halo_dic["r200_deg"]*un.deg, direction=0,
+    )
+    fimg_x_slice = fimg[:, len(fimg)//2]
+    fimg_y_slice = fimg[len(fimg)//2, :]
+    assert fimg_x_slice.max() == 0.
+    npt.assert_almost_equal(fimg_y_slice.max() * 1e7, 1.713281, decimal=5)
+    # test dipole filter along 1-direction
+    fimg = Filters.gaussian_third_derivative(
+        img, theta=theta*un.deg, theta_i=halo_dic["r200_deg"]*un.deg, direction=1,
+    )
+    fimg_x_slice = fimg[:, len(fimg)//2]
+    fimg_y_slice = fimg[len(fimg)//2, :]
+    assert fimg_x_slice.max() == 0.
+    npt.assert_almost_equal(fimg_y_slice.max() * 1e7, 8.210071, decimal=5)
 
